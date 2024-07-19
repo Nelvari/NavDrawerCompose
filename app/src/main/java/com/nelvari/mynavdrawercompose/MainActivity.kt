3320ad7ac80dad22030457fee816fac3c993dbbf
Nelvari <nelvari2018rplrus2@gmail.com>
2024-07-19 13:40:12 +0700

    initial commit

---
+++ app/src/main/java/com/nelvari/mynavdrawercompose/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.nelvari.mynavdrawercompose
+
+import MyNavDrawerApp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nelvari.mynavdrawercompose.ui.theme.MyNavDrawerComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyNavDrawerComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyNavDrawerApp()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    MyNavDrawerComposeTheme {
+
+    }
+}
